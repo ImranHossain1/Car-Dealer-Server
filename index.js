28f1a0647086e648863d8df9a13a5475b6617e8e
ImranHossain1 <imranhossain1402@gmail.com>
2022-07-28 16:34:33 +0200

    Final Deploy

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ async function run(){
      app.patch('/booking/:id',verifyJWT, async(req,res)=>{
       const id = req.params.id;
       const payment = req.body;
-      const filter ={_id : ObjectId(id)};;
+      const filter ={_id : ObjectId(id)};
       const updatedDoc = {
         $set : {
           paid: true,
